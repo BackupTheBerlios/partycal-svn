ee59ae222a5974bd253a2a6d4242ed6cf03278b5
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2006-12-21 19:36:42 +0000

    rewrite of Bootstrap for new layout

--- trunk/lib/PartyCalSetup.php
+++ trunk/lib/PartyCalSetup.php
@@ -1,16 +1,59 @@
 <?php
 /**
- * Basic PHP Configuration.
+ * Basic Bootstrap Configuration.
  * 
- * Add your include paths here if your on hosting. Actually hack at will.
+ * This script should get you running fast. You may coose to comment this 
+ * out and do most of it in php.ini and .profile, just these rules:
+ * 
+ * PARTYCAL_CONFIG must be set in your env.
+ * The following needs to get added to your include_path:
+ * - PARTYCAL_ROOT/lib
+ * - Zend Framework library/ dir.
+ * - PARTYCAL_ROOT/ext/providers/NEEDED_PROVIDERS
+ * - PARTYCAL_ROOT/ext/subscribers/NEEDED_SUBSCRIBERS
  *
- * @filesource
+ * You may choose to ignore $_ENV['PARTYCAL_ROOT'] if you want to it only 
+ * gets used here.
+ *
+ * Most users will just let this script make some informed guesses.
  */
 
-ini_set('include_path', '.:/home/hairmare/dev/php/zf/library/');
-
 if ( !isset( $_ENV['PARTYCAL_CONFIG'] ) ) {
-	$_ENV['PARTYCAL_CONFIG'] = 'config/partycal.ini';
+	$_ENV['PARTYCAL_CONFIG'] = $_ENV['PARTYCAL_ROOT'] . '/config/partycal.ini';
 }
+
+$partycal_extensions = '';
+$partycal_providers = dir( $_ENV['PARTYCAL_ROOT'] . '/ext/providers' );
+while ( ( $partycal_provider_item = $partycal_providers->read()) !== false ) {
+
+	if ( ( $partycal_provider_item == '.' )
+	||   ( $partycal_provider_item == '..' )
+	||   ( $partycal_provider_item == '.svn' ) )
+	continue;
+
+	$partycal_extensions .= $_ENV['PARTYCAL_ROOT'] 
+			     . '/ext/providers/'
+			     . $partycal_provider_item . ':';
+}
+$partycal_providers->close();
+
+$partycal_subscribers = dir( $_ENV['PARTYCAL_ROOT'] . '/ext/subscribers' );
+while ( ( $partycal_subscriber_item = $partycal_subscribers->read() ) !== false ) {
+
+	if ( ( $partycal_subscriber_item == '.' )
+	||   ( $partycal_subscriber_item == '..' )
+	||   ( $partycal_subscriber_item == '.svn' ) )
+	continue;
+
+	$partycal_extensions .= $_ENV['PARTYCAL_ROOT'] 
+			     . '/ext/providers/'
+			     . $partycal_subscriber_item . ':';
+}
+$partycal_subscribers->close();
+
+ini_set( 'include_path' , ini_get('include_path') . ':'
+			. $_ENV['PARTYCAL_ROOT'] . '/lib:'
+		        . $_ENV['PARTYCAL_ROOT'] . '/lib/zf:' 
+			. $partycal_extensions );
 
 ?>
