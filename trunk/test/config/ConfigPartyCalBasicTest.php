186e9b743944a1468db76523b248278a14fcbf50
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2006-12-24 20:57:11 +0000

    added test for config and cofnig validator

---
+++ trunk/test/config/ConfigPartyCalBasicTest.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+$_ENV['PARTYCAL_ROOT'] = '.';
+require_once 'lib/PartyCalBootstrap.php';
+require_once 'lib/Config.php';
+ 
+class ConfigPartyCalBasicTest extends PHPUnit_Framework_TestCase
+{
+	public $sut;
+
+	public function setUp()
+	{
+		$_ENV['PARTYCAL_CONFIG'] = $_ENV['PARTYCAL_ROOT'] . '/test/config/config/partycal-testlistings.ini';
+		$this->sut = new Config_PartyCal( 'partycal' );
+	}
+
+	public function tearDown()
+	{
+		unset( $this->sut );
+	}
+	
+	public function testGetDataReturnsProperArray()
+	{
+		$this->assertEquals( $this->sut->getData() 
+				   , array( 'db_dso' => 'sqlite://test/config/config/test.db' ) );
+	}
+
+	public function testGetListingReturnsArrayEmpty()
+	{
+		$this->assertEquals( $this->sut->getNodeListing( 'empty-listing' ) , array() );
+	}
+
+	public function testGetSubscriberListing()
+	{
+		$this->assertEquals( $this->sut->getSubscriberListing() 
+				   , array( 'test1' => 'active' 
+				          , 'test2' => 'inactive' 
+					  , 'test3' => 'error'
+					  )
+				   );
+	}
+
+	public function testGetProviderListing()
+	{
+		$this->assertEquals( $this->sut->getProviderListing() 
+				   , array( 'test1' => 'active' 
+				          , 'test2' => 'inactive' 
+					  , 'test3' => 'error'
+					  )
+				   );
+	}
+}
+
+?>
