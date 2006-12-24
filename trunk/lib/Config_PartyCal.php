b9195d5bf33b7981f9ae05183f936720c4fbd54e
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2006-12-24 11:47:46 +0000

    added documentation, requires and initial support for config validation

--- trunk/lib/Config_PartyCal.php
+++ trunk/lib/Config_PartyCal.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * Basic Configuration Handling
+ *
+ * @copyright Released under the GNU GPL, see LICENSE for more Information
+ * @author Lucas S. Bickel 
+ * @package config
  */
 
 /**
@@ -14,23 +18,89 @@ require_once 'Zend/Config.php';
 require_once 'Zend/Config/Ini.php';
 
 /**
+ * PartyCal Config Validator
+ */
+require_once 'Config/Validator.php';
+
+/**
+ * Generate Exceptions on Error.
+ */
+define( 'CONFIG_PARTYCAL_MODE_EXCEPTIONS' , 1 );
+
+/**
+ * Write Errors to stderr.
+ */
+define( 'CONFIG_PARTYCAL_MODE_STDERR' , 2 );
+
+/**
  * Configuration Handler for PartyCal
+ *
+ * @class
  */
 class Config_PartyCal extends Zend_Config {
 
-	public function __construct( $node )
+	/**
+	 * Open and validate a config file.
+	 *
+	 * uses CONFIG_PARTYCAL_MODE_EXCEPTIONS and CONFIG_PARTYCAL_MODE_STDERR to 
+	 * throw exceptions or output errors
+	 *
+	 * @throws Config_Exception_PartyCal
+	 *
+	 * @param $node String needed node
+	 * @param $mode Long mode for error handling
+	 *
+	 * @todo implement Exception/sdterr handling
+	 */
+	public function __construct( $node , $mode )
 	{
 		parent::__construct( new Zend_Config_Ini( $_ENV['PARTYCAL_CONFIG'], $node ) );
+
+		$config_validator = new Config_Validator_PartyCal ( $_ENV['PARTYCAL_CONFIG'] );
+		try {
+			$config_validator->validate();
+		} catch (Exception $e) {
+			//check mode
+			//write to stderr
+		}
 	}
 
+	/**
+	 * provider listing as an array
+	 *
+	 * @return Array subscriber listing
+	 */
 	static function getProviderListing() {
-		return new Config_PartyCal('provider-listing');
+		return Config_PartyCal::getNodeListing( 'provider-listing' );
 	}
 
+	/**
+	 * provider listing as an array
+	 *
+	 * @return Array subscriber listing
+	 */
 	static function getSubscriberListing() {
-		return new Config_PartyCal('subscriber-listing');
+		return Config_PartyCal::getNodeListing( 'subscriber-listing' );
 	}
 
+	/**
+	 * Return a ini node as an Array.
+	 *
+	 * @param $node String node name
+	 * @return Array
+	 */
+	static function getNodeListing( $node )
+	{
+		$n = new Config_PartyCal( $node );
+
+		return $n->getData();
+	}
+
+	/**
+	 * expose internal _data Array
+	 *
+	 * @return Array (array) cast of $this->_data
+	 */
 	public function getData()
 	{
 		return (array) $this->_data;
