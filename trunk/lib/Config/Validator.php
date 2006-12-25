5a658ea47af9e9c1c8eb8a79907473e836b49590
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2006-12-25 10:42:42 +0000

    config validator internal api, some docs, constructor

--- trunk/lib/Config/Validator.php
+++ trunk/lib/Config/Validator.php
@@ -15,18 +15,53 @@
  */
 
 /**
+ *
+ */
+define('CONFIG_VALIDATOR_PARTYCAL_MODE_SCAN', 1);
+
+/**
  * Config Validation Class
  */
 class Config_Validator_PartyCal {
 
 	/**
-	 *
-	 * @todo write construct method
-	 * @todo add support for dynamic loading/registering validators like Provider_Petzi_Config_Validator_Node_PartyCal
+	 * @var Config_PartyCal Config Object being validated.
 	 */
-	public function __construct( $config )
+	public $config;
+
+	/**
+	 * @var Array chain of Config_Validator_Nodes/Classnames.
+	 */
+	public $validatorchain;
+
+	/**
+	 * @var Array Dirs to scan in loadValidatorsByDir.
+	 */
+	public $validatorscandirs = array( 'ext/providers/' , 'ext/subscribers' );
+
+	/**
+	 * Store config and load validators.
+	 *
+	 * Basic validation comes from registerCoreValidators(). you can choose to let Config_Validator 
+	 * do its own scanning in the dirs in $this->validatorscandirs. 
+	 * $flag may be:
+	 * - CONFIG_VALIDATOR_PARTYCAL_MODE_SCAN
+	 *
+	 * @param Config_PartyCal $config
+	 * @param Array $validators Validators to load
+	 * @param Flag $flag more settings
+	 */
+	public function __construct( $config , $validators = NULL, $flag = 0 )
 	{
-		
+		$this->config = $config;
+
+		$this->registerCoreValidators();
+
+		if ( $scan ) {
+			$this->loadValidatorsByDir( $this->validatorscandirs );
+		}
+
+		$this->append($validators);
 	}
 
 	/**
@@ -38,6 +73,50 @@ class Config_Validator_PartyCal {
 	 */
 	public function validate()
 	{
+	}
+
+	/**
+	 * Register the core Validators.
+	 *
+	 * Core validators get included manually at the top of this file.
+	 *
+	 * @todo implement
+	 */
+	public function registerCoreValidators()
+	{
+	}
+
+	/**
+	 * 
+	 *
+	 * @param Array $scandirs Dirs to scan for Config/Validator subdirectories
+	 *
+	 * @todo implement support for dynamic loading/registering validators like Provider_Petzi_Config_Validator_Node_PartyCal
+	 */
+	public function loadValidatorsByDir( Array $scandirs )
+	{
+	}
+
+	public function append($validators)
+	{
+		if ( is_string($validators) ) {
+			$validators = array ( $validators );
+		}
+
+		if ( !empty( $this->validatorchain ) ) {
+			$this->validatorchain = array_merge( $this->validatorchain , $validators );
+		} else {
+			$this->validatorchain = $validators;
+		}
+	}
+
+	public function prepend($validators)
+	{
+		if ( !empty( $this->validatorchain ) ) {
+			$this->validatorchain = array_merge( $validators , $this->validatorchain );
+		} else {
+			$this->validatorchain = $validators;
+		}
 	}
 }
 
