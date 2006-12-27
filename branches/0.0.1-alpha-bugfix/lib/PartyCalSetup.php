b22ced9380b9891a2da7d9a27e931001d22f1e6a
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2006-12-27 19:57:31 +0000

    hardcoded ini file for now, this branch gets used by me and aint merging anywhere soon

--- branches/0.0.1-alpha-bugfix/lib/PartyCalSetup.php
+++ branches/0.0.1-alpha-bugfix/lib/PartyCalSetup.php
@@ -3,7 +3,7 @@
 ini_set('include_path', '.:/home/hairmare/dev/php/zf/library/');
 
 if ( !isset( $_ENV['PARTYCAL_CONFIG'] ) ) {
-	$_ENV['PARTYCAL_CONFIG'] = 'config/partycal.ini';
+	$_ENV['PARTYCAL_CONFIG'] = '/home/hairmare/partycal/config/partycal.ini';
 }
 
 ?>
