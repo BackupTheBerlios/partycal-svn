6f11d5ec2a2fe3c0d1e7438964e3ae62e45da282
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2007-12-29 17:35:28 +0000

    added some stuff so cc integration barely works

---
+++ branches/nu-wiki-sync/source/tests/core.php
@@ -0,0 +1,13 @@
+<?php
+
+class test_core extends PHPUnit_Framework_TestCase {
+
+	// this tests is quite cheap, it makes shure the whole harness works though.
+	function testTrue()
+	{
+		$this->assertTrue(true);
+	}
+
+}
+
+?>
