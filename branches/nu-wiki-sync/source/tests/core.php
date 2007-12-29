694c4d713747754eef35277736bdc896297277d8
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2007-12-29 18:12:38 +0000

    more cs stuff

--- branches/nu-wiki-sync/source/tests/core.php
+++ branches/nu-wiki-sync/source/tests/core.php
@@ -1,32 +1,41 @@
 <?php
 /**
  * You may find some tests looking for php features in here.
+ *
+ * @category Tests
+ * @package  Partycal
+ * @author   Lucas S. Bickel <hairmare@gmail.com>
+ * @license  GPL
+ * @link     http://partycal.berlios.de
  */
 
 
 /**
  * actual core tests.
  *
+ * @class 
  */
 class Partycal_Test_Core extends PHPUnit_Framework_TestCase 
 {
 
-	// this tests is quite cheap, it makes shure the whole harness works though.
-	function testTrue()
-	{
-		$this->assertTrue(true);
-	}
+    function testTrue()
+    {
+        $this->assertTrue(true);
+    }
 
-	function testSqliteInstalled()
-	{
-		$this->assertTrue(function_exists('sqlite_open'));
-	}
+    /**
+     * check if sqlite is installed
+     */
+    function testSqliteInstalled()
+    {
+        $this->assertTrue(function_exists('sqlite_open'));
+    }
 
-	function testStreamWrapperHttpExists()
-	{
-		$this->assertTrue(in_array('http', stream_get_wrappers()));
-		$this->assertTrue(in_array('https', stream_get_wrappers()));
-	}
+    function testStreamWrapperHttpExists()
+    {
+        $this->assertTrue(in_array('http', stream_get_wrappers()));
+        $this->assertTrue(in_array('https', stream_get_wrappers()));
+    }
 
 }
 
