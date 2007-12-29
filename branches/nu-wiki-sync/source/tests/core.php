66541def279ad4c0eaa4c7783cfaff830f2b5dc2
hairmare <hairmare@f41c42cb-0725-0410-88fc-c481f7ac364b>
2007-12-29 18:29:56 +0000

    old version was better, cc integration only needs a runner and more tests and we are go

--- branches/nu-wiki-sync/source/tests/core.php
+++ branches/nu-wiki-sync/source/tests/core.php
@@ -16,6 +16,7 @@
  * actual core tests.
  *
  * @category Tests
+ * @package  Partycal
  * @author   Lucas S. Bickel <hairmare@gmail.com>
  * @license  http://www.gnu.org/copyleft/gpl.html GNU GENERAL PUBLIC LICENSE
  * @link     http://partycal.berlios.de
